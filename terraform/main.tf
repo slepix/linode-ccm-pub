4ef8455ae29493b80055e3a5635a4c6f7c69fe22
Alesandro Slepcevic <8218365+slepix@users.noreply.github.com>
2026-03-15 00:24:34 +0100

    Updated main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -115,7 +115,7 @@ resource "linode_instance" "app" {
 
   interface {
     purpose   = "vpc"
-    subnet_id = linode_vpc_subnet.main.id
+    subnet_id = linode_vpc_subnet.app.id
     ipv4 {
       vpc     = var.instance_vpc_ip
       nat_1_1 = "any"
